d051811680f9f500644afd7026330e7595f2d313
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-24 21:17:41 +0100

    Configuring logins

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-
+import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';   
 const Login: React.FC = () => {
-
+  const doLogin = (event: any) => {
+    event.preventDefault()
+    console.log('doLogin') // Fake login
+  } 
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
+        <IonToolbar color={'success'}>
+          <IonTitle>My App Title</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        UI goes here...
+        <IonCard>
+          <IonCardContent>
+            asd
+          </IonCardContent>
+        </IonCard>
       </IonContent>
+      <IonFooter>
+        <IonToolbar>asdas</IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
