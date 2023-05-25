b065d5ef51412fe20c7d11bed796a5e3908296ca
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-25 10:17:11 +0100

    Fixed restart errors

--- dist/assets/md.transition-legacy-86188ef7.js
+++ dist/assets/md.transition-legacy-86188ef7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4322543d.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.l,i=e.k}],execute:function(){
+System.register(["./index-legacy-48766dc9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.l,i=e.k}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
