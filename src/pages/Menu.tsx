d5a6567afe90abb32567efe240edf06873520e0b
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-28 11:56:45 +0100

    List, Menu, Settings

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,18 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import{ Redirect, Route } from 'react-router'
+import Settings from './Settings';
+import List from './List';
+import { homeOutline, newspaperOutline } from 'ionicons/icons';
 
 const Menu: React.FC = () => {
+  const paths = [
+    { name: 'Home', url: 'app/list', icon: homeOutline },
+    { name: 'Settings', url: '/app/settings', icon: newspaperOutline }
+]
+
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        UI goes here...
-      </IonContent>
+        <IonMenu contentId='main'>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>
+                Menu
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className='ion-padding'>
+            {paths.map((path, index) => (
+              <IonMenuToggle>
+                <IonItem routerLink={path.url} key={index} routerDirection="none">
+                  {IonItem.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
+
+            
+          </IonContent>
+        </IonMenu>
+
+        <IonRouterOutlet id='main'>
+          <Route exact path="/app/list" component={List} />
+          <Route path="/app/settings" component={Settings} />
+          <Route exact path = "/app">
+            <Redirect to="/app/list" />
+          </Route>
+        </IonRouterOutlet>
     </IonPage>
   );
 };
