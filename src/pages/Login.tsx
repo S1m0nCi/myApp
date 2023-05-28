1e3cf1a8923eca10663c3beab839eb101316ac20
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-28 11:06:16 +0100

    Login updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,39 @@
 import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, useIonRouter } from '@ionic/react';
-import React, { useState } from 'react';   
+import React, { useEffect, useState } from 'react';   
 import { logInOutline, personCircleOutline } from 'ionicons/icons'
 import bikemoney from '../assets/bike-money.png'
 import Intro from '../components/Intro';
+import { Preferences } from '@capacitor/preferences';
+
+const INTRO_KEY = 'intro-seen'
 
 const Login: React.FC = () => {
   const router = useIonRouter()
-  const [introSeen, setIntroSeen] = useState(false)
+  const [introSeen, setIntroSeen] = useState(true)
+  
+  useEffect(() => {
+    const checkStorage = async () => {
+      const seen =  await Preferences.get({ key: INTRO_KEY });
+      console.log('check storage ~ seen')
+      setIntroSeen(seen.value === 'true')
+    };
+    checkStorage();
+  })
+
   const doLogin = (event: any) => {
     event.preventDefault()
     console.log('doLogin') // Fake login
-    // router.push('/home', 'root')
+    router.push('/app', 'root')
   } 
 
   const finishIntro = async () => {
-    console.log('FINISHED');
     setIntroSeen(true);
+    Preferences.set({ key: INTRO_KEY, value: 'true' })
+  }
+
+  const seeIntroAgain = () => {
+    setIntroSeen(false)
+    Preferences.remove({ key: INTRO_KEY })
   }
 
   return (
@@ -45,6 +63,10 @@ const Login: React.FC = () => {
               </IonButton>
               <IonButton routerLink='/register' color='secondary' type='button' expand='block' className='ion-margin-top'>
                 Create account
+                <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
+              </IonButton>
+              <IonButton onClick={seeIntroAgain} fill="clear" size="small" routerLink='/register' color='medium' type='button' expand='block' className='ion-margin-top'>
+                Watch Intro
                 <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
               </IonButton>
             </form>
