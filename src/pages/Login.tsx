969d5de07d94a1153f411441ee23287feec7ed60
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-28 11:24:09 +0100

    First Parts completed and centred

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, useIonRouter } from '@ionic/react';
+import { IonRow, IonCol, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, useIonRouter, useIonLoading, IonGrid } from '@ionic/react';
 import React, { useEffect, useState } from 'react';   
 import { logInOutline, personCircleOutline } from 'ionicons/icons'
 import bikemoney from '../assets/bike-money.png'
@@ -10,6 +10,7 @@ const INTRO_KEY = 'intro-seen'
 const Login: React.FC = () => {
   const router = useIonRouter()
   const [introSeen, setIntroSeen] = useState(true)
+  const [present, dismiss] = useIonLoading()
   
   useEffect(() => {
     const checkStorage = async () => {
@@ -18,13 +19,16 @@ const Login: React.FC = () => {
       setIntroSeen(seen.value === 'true')
     };
     checkStorage();
-  })
+  });
 
-  const doLogin = (event: any) => {
-    event.preventDefault()
-    console.log('doLogin') // Fake login
-    router.push('/app', 'root')
-  } 
+  const doLogin = async (event: any) => {
+    event.preventDefault();
+    await present('Logging in...');
+    setTimeout(async () => {
+      dismiss()
+      router.push('/app', 'root');
+    }, 2000)
+  }; 
 
   const finishIntro = async () => {
     setIntroSeen(true);
@@ -48,30 +52,42 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent scrollY={false}>
-        <img src={bikemoney} alt='Money Man Bike' width={ '50%' } />
-        <IonCard>
-          <IonCardContent>
-            <form onSubmit={doLogin}> {/*type='email' tells the native OS 
-            to display the email keyboard, and type='password' means
-            that we have asterisks instead of text.*/}
-              <IonInput fill="outline" labelPlacement='floating' label="Email" type='email' placeholder='simon@myapp.com'></IonInput>
-              <IonInput className="ion-margin-top" fill="outline" labelPlacement='floating' label="Password" type='password' placeholder='abc123'></IonInput>
-              <IonButton type='submit' expand='block' className='ion-margin-top'>
-                Login
-                <IonIcon icon={logInOutline} slot='end'></IonIcon>
-              </IonButton>
-              <IonButton routerLink='/register' color='secondary' type='button' expand='block' className='ion-margin-top'>
-                Create account
-                <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
-              </IonButton>
-              <IonButton onClick={seeIntroAgain} fill="clear" size="small" routerLink='/register' color='medium' type='button' expand='block' className='ion-margin-top'>
-                Watch Intro
-                <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
+      <IonContent scrollY={false} className='ion-padding'>
+        <IonGrid fixed>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4"> 
+              <img src={bikemoney} alt='Money Man Bike' width={ '50%' } />
+            </IonCol>
+          </IonRow>
+
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <IonCard>
+                <IonCardContent>
+                  <form onSubmit={doLogin}> {/*type='email' tells the native OS 
+                  to display the email keyboard, and type='password' means
+                  that we have asterisks instead of text.*/}
+                    <IonInput fill="outline" labelPlacement='floating' label="Email" type='email' placeholder='simon@myapp.com'></IonInput>
+                    <IonInput className="ion-margin-top" fill="outline" labelPlacement='floating' label="Password" type='password' placeholder='abc123'></IonInput>
+                    <IonButton type='submit' expand='block' className='ion-margin-top'>
+                      Login
+                      <IonIcon icon={logInOutline} slot='end'></IonIcon>
+                    </IonButton>
+                    <IonButton routerLink='/register' color='secondary' type='button' expand='block' className='ion-margin-top'>
+                      Create account
+                      <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
+                    </IonButton>
+                    <IonButton onClick={seeIntroAgain} fill="clear" size="small" routerLink='/register' color='medium' type='button' expand='block' className='ion-margin-top'>
+                      Watch Intro
+                      <IonIcon icon={personCircleOutline} slot='end'></IonIcon>
+                    </IonButton>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+ 
+        </IonGrid>
       </IonContent>
       <IonFooter>
         <IonToolbar>.</IonToolbar>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { IonButtons, IonFooter, IonButton, IonInput, IonIcon, IonCardContent, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonBackButton, useIonRouter } from '@ionic/react';
+import { IonRow, IonCol, IonButtons, IonFooter, IonButton, IonInput, IonIcon, IonCardContent, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonBackButton, useIonRouter, IonGrid } from '@ionic/react';
 import React from 'react';
 import { checkmarkDoneOutline, logInOutline, personCircleOutline } from 'ionicons/icons'
 import bikemoney from '../assets/bike-money.png'
@@ -23,22 +23,28 @@ const Register: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent scrollY={false}>
+      <IonContent scrollY={false} className='ion-padding'>
         <img src={bikemoney} alt='Money Man Bike' width={ '50%' } />
-        <IonCard>
-          <IonCardContent>
-            <form onSubmit={doRegister}> {/*type='email' tells the native OS 
-            to display the email keyboard, and type='password' means
-            that we have asterisks instead of text.*/}
-              <IonInput fill="outline" labelPlacement='floating' label="Email" type='email' placeholder='simon@myapp.com'></IonInput>
-              <IonInput className="ion-margin-top" fill="outline" labelPlacement='floating' label="Password" type='password' placeholder='abc123'></IonInput>
-              <IonButton type='submit' expand='block' className='ion-margin-top'>
-                Create account
-                <IonIcon icon={checkmarkDoneOutline} slot='end'></IonIcon>
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
+        <IonGrid>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <IonCard>
+                <IonCardContent>
+                  <form onSubmit={doRegister}> {/*type='email' tells the native OS 
+                  to display the email keyboard, and type='password' means
+                  that we have asterisks instead of text.*/}
+                    <IonInput fill="outline" labelPlacement='floating' label="Email" type='email' placeholder='simon@myapp.com'></IonInput>
+                    <IonInput className="ion-margin-top" fill="outline" labelPlacement='floating' label="Password" type='password' placeholder='abc123'></IonInput>
+                    <IonButton type='submit' expand='block' className='ion-margin-top'>
+                      Create account
+                      <IonIcon icon={checkmarkDoneOutline} slot='end'></IonIcon>
+                    </IonButton>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
       <IonFooter>
         <IonToolbar>.</IonToolbar>
